31a0dcea0c97575ec0982bec0a75abb7f38101cc
Aron-tech <aron.papp2003@gmail.com>
2025-06-24 12:34:13 +0200

    add: freedom_expiring column

--- app/Models/Guild.php
+++ app/Models/Guild.php
@@ -30,7 +30,7 @@ class Guild extends Model
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class)
-            ->withPivot('ic_name', 'ic_number', 'ic_tel', 'last_role_time', 'last_warn_time', 'created_at');
+            ->withPivot('ic_name', 'ic_number', 'ic_tel', 'last_role_time', 'last_warn_time', 'created_at', 'freedom_expiring');
     }
 
     public function usersWithOutPivot(): BelongsToMany
