8a20ce608c883bb5f635d8063f92e60ad235567c
Aron-tech <aron.papp2003@gmail.com>
2025-06-24 19:21:26 +0200

    add: freedom function

--- resources/views/livewire/pages/dashboard.blade.php
+++ resources/views/livewire/pages/dashboard.blade.php
@@ -1,47 +1,98 @@
 <?php
 
+use App\Enums\PermissionEnum;
+use App\Livewire\Traits\FormatsDuty;
 use App\Models\Guild;
 use App\Models\GuildSelector;
 use App\Models\User;
 use Carbon\Carbon;
 use Livewire\Volt\Component;
 use Livewire\Attributes\{Layout, Title};
+use TallStackUi\Traits\Interactions;
+use Livewire\Attributes\Computed;
 
 new
 #[Layout('layouts.app')]
 #[Title('Információk')]
 class extends Component {
 
+    use Interactions;
+    use FormatsDuty;
+
     public ?Guild $guild = null;
 
-    public ?User $user = null;
-
-    public string $total_duty = '0:0';
-    public string $period_duty = '0:0';
-
-    public int $days_in_role = 0;
-    public int $days_in_faction = 0;
+    public ?string $freedom_date = null;
 
     public function mount()
     {
         $this->guild = GuildSelector::getGuild();
 
-        if($this->guild->users()->where('discord_id', auth()->id())->exists())
-            $this->updateData();
-        elseif(auth()->user()->can('hasPermission', [$this->guild, \App\Enums\PermissionEnum::VIEW_ADMIN_PANEL]))
-            return to_route('admin.install');
-        else
+        $user = $this->guild->users()->where('discord_id', auth()->id())->first() ?? null;
+
+        if (!isset($user) && auth()->user()->can('hasPermission', [$this->guild, PermissionEnum::VIEW_SETTINGS]))
+            return to_route('admin.settings');
+        elseif(!isset($user))
             return to_route('guild.selector');
+
+        $pivot = $user?->pivot ?? null;
+    
+
+        $this->freedom_date = $pivot->freedom_expiring
+            ? Carbon::parse($pivot->freedom_expiring)->format('Y-m-d')
+            : null;
     }
 
-    public function updateData()
+    /*#[Computed]
+    public function user(): ?User
+    {
+        return $this->guild
+            ? $this->guild->users()
+                ->where('discord_id', auth()->id())
+                ->first()
+            : null;
+    }*/
+
+    /*public function updateData(): void
     {
         if ($this->guild) {
-            $this->user = $this->guild->users()->where('discord_id', auth()->id())->first();
-            $this->period_duty = dutyTimeFormatter($this->user->periodDutyTime($this->guild));
-            $this->total_duty = dutyTimeFormatter($this->user->totalDutyTime($this->guild));
-            $this->days_in_role = Carbon::parse($this->user->pivot->last_role_time)->diffInDays(now());
-            $this->days_in_faction = Carbon::parse($this->user->pivot->created_at)->diffInDays(now());
+            $this->period_duty = $this->formatMinutesToHHMM($this->user()->periodDutyTime($this->guild));
+            $this->total_duty = $this->formatMinutesToHHMM($this->user()->totalDutyTime($this->guild));
+            $this->days_in_role = Carbon::parse($this->user()->pivot->last_role_time)->diffInDays(now());
+            $this->days_in_faction = Carbon::parse($this->user()->pivot->created_at)->diffInDays(now());
+            $this->freedom_date = $this->user()->pivot->freedom_expiring
+                ? Carbon::parse($this->user()->pivot->freedom_expiring)->format('Y-m-d')
+                : null;
+        }
+    }*/
+
+    public function with(): array
+    {
+        $user = $this->guild->users()
+            ->where('discord_id', auth()->id())
+            ->withSum('duties as period_duty', 'value')
+            ->first();
+
+        $user->total_duty = $user->totalDutyTime($this->guild);
+
+        $days_in_role = (int)Carbon::parse($user->pivot->last_role_time)->diffInDays(now());
+        $days_in_faction = (int)Carbon::parse($user->pivot->created_at)->diffInDays(now());
+
+        return [
+            'user' => $user,
+            'days_in_role' => $days_in_role,
+            'days_in_faction' => $days_in_faction,
+        ];
+    }
+
+    public function saveFreedomDate(): void
+    {
+        if ($this->freedom_date) {
+            DB::table('guild_user')->where('guild_guild_id', $this->guild->guild_id)
+                ->where('user_discord_id', auth()->id())
+                ->update(['freedom_expiring' => $this->freedom_date]);
+            $this->toast()->success('Szabadság dátuma frissítve!')->send();
+        } else {
+            $this->toast()->error('Kérlek adj meg egy érvényes dátumot!')->send();
         }
     }
 }; ?>
@@ -52,14 +103,21 @@ class extends Component {
         <x-h4 class="my-auto dark:text-white uppercase">{{$user->name}}</x-h4>
     </div>
     <div class="flex my-10 gap-2">
-        <x-stats wire:click="updateData" :number="$period_duty" title="Aktuális szolgálati idő" icon="clock"/>
-        <x-stats wire:click="updateData" :number="$total_duty" title="Összes szolgálati idő" icon="clock"/>
-        <x-stats wire:click="updateData" :number="$days_in_role" animated title="Rangon eltöltött napok száma" icon="calendar-days"/>
-        <x-stats wire:click="updateData" :number="$days_in_faction" animated title="Frakcióban eltöltött napok száma" icon="calendar-date-range"/>
+        <x-stats :number="dutyTimeFormatter((int) $user->period_duty)" title="Aktuális szolgálati idő" icon="clock"/>
+        <x-stats :number="dutyTimeFormatter($user->total_duty)" title="Összes szolgálati idő" icon="clock"/>
+        <x-stats :number="$days_in_role" animated title="Rangon eltöltött napok száma"
+                 icon="calendar-days"/>
+        <x-stats :number="$days_in_faction" animated title="Frakcióban eltöltött napok száma"
+                 icon="calendar-date-range"/>
     </div>
-    <div class="grid lg:grid-cols-3 grid-cols-1 gap-4 my-10">
-        <x-input label="IC név" :value="$user->pivot->ic_name" readonly/>
-        <x-input label="Jelvényszám" :value="$user->pivot->ic_number" readonly/>
-        <x-input label="Telefonszám" :value="$user->pivot->ic_tel" readonly/>
-    </div>
+    @if($user->pivot)
+        <div class="grid lg:grid-cols-4 grid-cols-1 gap-4 my-10">
+            <x-input label="IC név" :value="$user->pivot->ic_name" readonly/>
+            <x-input label="Jelvényszám" :value="$user->pivot->ic_number" readonly/>
+            <x-input label="Telefonszám" :value="$user->pivot->ic_tel" readonly/>
+            <x-date wire:model.live="freedom_date" label="Szabadságon eddig" :min-date="now()"
+                    :max-date="now()->addYear()"
+                    wire:change="saveFreedomDate"/>
+        </div>
+    @endif
 </div>
