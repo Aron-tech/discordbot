085d418c1678da0fd022feffa8a1daf9a183543f
Aron-tech <aron.papp2003@gmail.com>
2025-06-25 07:56:31 +0200

    feat: add caching

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Guild;
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    public function index(){
-        $guild_count = Guild::count();
-        $user_count = User::count();
-        return view('welcome', compact('guild_count','user_count'));
+    public function index()
+    {
+        $guild_count = cache()->remember('guild_count', 30, function () {
+            return Guild::count();
+        });
+        $user_count = cache()->remember('user_count', 30, function () {
+            return User::count();
+        });
+
+        return view('welcome', compact('guild_count', 'user_count'));
     }
 }
