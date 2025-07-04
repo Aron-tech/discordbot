174de2b89fae83c8da5f89f26db146459cb4905c
Aron-tech <aron.papp2003@gmail.com>
2025-07-04 23:15:01 +0200

    add: exam result model and migration

---
+++ app/Models/ExamResult.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ExamResult extends Model
+{
+    protected $fillable = [
+        'exam_id',
+        'user_discord_id',
+        'score',
+        'passed',
+    ];
+
+    protected $casts = [
+        'passed' => 'boolean',
+    ];
+
+    public function exam(): BelongsTo
+    {
+        return $this->belongsTo(Exam::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2025_07_02_133522_create_exam_results_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Exam;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exam_results', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Exam::class)->constrained();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->unsignedInteger('score');
+            $table->boolean('passed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exam_results');
+    }
+};
