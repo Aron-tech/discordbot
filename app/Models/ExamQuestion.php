16e93fea41df7857b4e1ae5c7a773774294e28a5
Aron-tech <aron.papp2003@gmail.com>
2025-07-04 23:22:33 +0200

    add: question model and migration

---
+++ app/Models/ExamQuestion.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class ExamQuestion extends Model
+{
+    protected $fillable = [
+        'exam_id',
+        'question',
+    ];
+
+    public function exam(): BelongsTo
+    {
+        return $this->belongsTo(Exam::class);
+    }
+
+    public function answers(): HasMany
+    {
+        return $this->hasMany(ExamAnswer::class);
+    }
+}
---
+++ database/migrations/2025_07_04_230851_create_exam_questions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Exam;
+use App\Models\ExamQuestion;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exam_questions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Exam::class)->constrained();
+            $table->text('question');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exam_questions');
+    }
+};
