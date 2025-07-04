c60f85e76fa1cd5d66de867ae2a8315dec7aa8a3
Aron-tech <aron.papp2003@gmail.com>
2025-07-04 23:35:29 +0200

    delete: unused import

--- database/migrations/2025_07_02_130500_create_exam_questions_table.php
+++ database/migrations/2025_07_02_130500_create_exam_questions_table.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Models\Exam;
-use App\Models\ExamQuestion;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
