bd7c630771a849f0d448bcb16ae243958f323643
Aron-tech <aron.papp2003@gmail.com>
2025-07-21 11:12:53 +0200

    add: blacklist page to sidebar

--- routes/admin.php
+++ routes/admin.php
@@ -5,7 +5,7 @@ use Livewire\Volt\Volt;
 
 Route::prefix('admin/')->name('admin.')->group(function () {
 
-    //Volt::route('statistics', 'admin.statistics')->name('statistics');
+    // Volt::route('statistics', 'admin.statistics')->name('statistics');
 
     Volt::route('settings', 'admin.settings')->name('settings')->middleware('check.permission:view_settings');
 
@@ -18,4 +18,6 @@ Route::prefix('admin/')->name('admin.')->group(function () {
     Volt::route('exam', 'admin.exam-manager')->name('exam-manager')->middleware('check.permission:view_exam_manager');
 
     Volt::route('exam/log', 'admin.exam-result')->name('exam-results')->middleware('check.permission:view_exam_result');
+
+    Volt::route('blacklist', 'admin.blacklist')->name('blacklist')->middleware('check.permission:view_blacklist');
 });
