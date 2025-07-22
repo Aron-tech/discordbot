d90b651972bdff8d5b9db9a12edd87d0e94f7262
Aron-tech <aron.papp2003@gmail.com>
2025-07-22 08:45:32 +0200

    add: soft delete to guilds

--- app/Models/Guild.php
+++ app/Models/Guild.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Guild extends Model
 {
+    use SoftDeletes;
     protected $primaryKey = 'guild_id';
     protected $keyType = 'string';
     public $incrementing = false;
