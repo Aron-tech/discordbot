d936113311dc532a16ea8afddc854ed566c92935
Aron-tech <aron.papp2003@gmail.com>
2025-07-30 15:19:33 +0200

    add: Hungarian language files

---
+++ lang/hu/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Bejelentkezési nyelvi sorok
+    |--------------------------------------------------------------------------
+    |
+    | Az alábbi nyelvi sorokat a hitelesítés során használjuk különféle
+    | üzenetek megjelenítésére. Szabadon módosíthatod őket az alkalmazásod
+    | igényei szerint.
+    |
+    */
+
+    'failed' => 'A megadott adatok nem egyeznek a nyilvántartásunkkal.',
+    'password' => 'A megadott jelszó hibás.',
+    'throttle' => 'Túl sok bejelentkezési kísérlet. Próbáld újra :seconds másodperc múlva.',
+
+];
---
+++ lang/hu/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Jelszó visszaállítás nyelvi sorok
+    |--------------------------------------------------------------------------
+    |
+    | Az alábbi nyelvi sorokat a jelszó visszaállítás során használjuk.
+    | Ezek az alapértelmezett üzenetek, amelyeket a rendszer küld hibák,
+    | vagy sikeres műveletek esetén, például érvénytelen token esetén.
+    |
+    */
+
+    'reset' => 'A jelszavad sikeresen vissza lett állítva.',
+    'sent' => 'Elküldtük a jelszó-visszaállító linket e-mailben.',
+    'throttled' => 'Kérlek, várj egy kicsit az újrapróbálkozás előtt.',
+    'token' => 'Ez a jelszó-visszaállító token érvénytelen.',
+    'user' => 'Nem található felhasználó ezzel az e-mail címmel.',
+
+];
