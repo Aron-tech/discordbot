30ffbab5a4340d1046daf6a6a09e5feee312db1e
Aron-tech <aron.papp2003@gmail.com>
2025-08-21 19:31:26 +0200

    add systems enum variables

--- app/Enums/Guild/SettingTypeEnum.php
+++ app/Enums/Guild/SettingTypeEnum.php
@@ -7,7 +7,18 @@ enum SettingTypeEnum: string
     case MIN_RANK_UP_DUTY = 'rank_up_duty';
     case MIN_RANK_UP_TIME = 'rank_up_time';
 
+    case NEXT_CHECKING_TIME = 'next_checking_time';
+
     case MIN_DUTY = 'min_duty';
     case WARN_TIME = 'warn_time';
+
+    //Systems boolean
+    case DUTY_SYSTEM = 'duty_system';
+    case WARN_SYSTEM = 'warn_system';
+    case RANK_SYSTEM = 'rank_system';
+    case HOLIDAY_SYSTEM = 'holiday_system';
+    case CHECK_SYSTEM = 'check_system';
+
+
 }
 
