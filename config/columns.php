cf7205d2bf9e5087952ba97460814170a57d6c5e
Aron-tech <aron.papp2003@gmail.com>
2025-09-09 18:29:39 +0200

    add visible columns config

---
+++ config/columns.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'admin_panel' => [
+        'name' => 'DC Név',
+        'ic_name' => 'IC Név',
+        'duties_sum_value' => 'Szolgálati idő',
+        'duties_with_trashed_sum_value' => 'Összes sz. idő',
+        'in_role_days' => 'Rangon',
+        'in_guild_days' => 'Frakcióban',
+        'duties_max_start_time' => 'Utolsó szolgálatba lépés ideje',
+        'status' => 'Státusz',
+    ],
+];
